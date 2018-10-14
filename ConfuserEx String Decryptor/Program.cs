abcd82f4f6bb023150572a6844aef6ddd9b31a5c
cawk-untested <41116996+cawk-untested@users.noreply.github.com>
2018-10-14 15:35:08 +0100

    First commit

---
+++ ConfuserEx String Decryptor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dnlib.DotNet;
+using dnlib.DotNet.Writer;
+
+namespace ConfuserEx_String_Decryptor
+{
+    class Program
+    {
+        public static ModuleDefMD Module { get; private set; }
+
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+            Module = ModuleDefMD.Load(path);
+            StringDecryptor.Deobfuscate();
+            ModuleWriterOptions modOpts = new ModuleWriterOptions(Module);
+            modOpts.MetadataOptions.Flags = MetadataFlags.PreserveAll;
+            Module.Write(path+"Cleaned.exe",modOpts);
+        }
+    }
+}
